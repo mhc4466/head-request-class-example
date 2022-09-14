062b21e7cf90a23ff47c52632d2ecf5c186c2f3f
Michael Chan (RIT Student) <mhc4466@rit.edu>
2022-09-14 13:52:55 -0400

    Finished class demo, missed something so get requests don't print to the client properly

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -13,19 +13,30 @@ const respondJSON = (request, response, status, object) => {
 };
 
 const respondJSONMeta = (request, response, status) => {
-
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
-
+  const responseJSON = {
+    users,
+  };
 };
 
 const getUsersMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 200);
 };
 
 const updateUser = (request, response) => {
-
+  const newUser = {
+    createdAt: Date.now(),
+  };
+  users[newUser.createdAt] = newUser;
+  return respondJSON(request, response, 201, newUser);
 };
 
 const notFound = (request, response) => {
@@ -38,7 +49,7 @@ const notFound = (request, response) => {
 };
 
 const notFoundMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 404);
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -6,20 +6,40 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  '/': htmlHandler.getIndex,
-  notFound: jsonHandler.notFound,
+  'GET': {
+    '/': htmlHandler.getIndex,
+    '/updateUser': jsonHandler.updateUser,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    notFound: jsonHandler.notFound,
+  },
+  'HEAD': {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFound,
+  },
 }
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
+  console.log(request.method);
+  console.log(parsedUrl.pathname);
 
+  /*
   if(urlStruct[parsedUrl.pathname]){
     urlStruct[parsedUrl.pathname](request, response);
   } else {
     urlStruct.notFound(request, response);
   }
+  */
+  if (!urlStruct[request.method]) {
+    return urlStruct['HEAD'].notFound(request, response);
+  }
 
-
+  if(urlStruct[request.method][parsedUrl.pathname]){
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
